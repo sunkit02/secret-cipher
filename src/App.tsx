c9fd31639f6189ea060eeafac24f48abdeca4113
Sun Kit Tsui <sunkittsui@gmail.com>
2022-12-20 01:41:06 -0500

    Committed fixed working project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {Route, Routes} from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+
+import Encoder from "./components/encoder/Encoder";
+import Message from "./components/message/Message";
+
+import "./App.css";
+import "./css/header.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+
+
+    return (
+        <>
+            <Header
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+                username={username}
+                setUsername={setUsername}
+            />
+            <div className="body">
+                <Routes>
+                    <Route index element={<Home/>}/>
+                    <Route path={"/encoder"} element={<Encoder/>}/>
+                    <Route path={"/message"} element={<Message/>}/>
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/encoder/Encoder.tsx
+++ src/components/encoder/Encoder.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Encoder: React.FC = ({}) => {
   return (
     <>
