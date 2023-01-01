2151363e2ac98904dc2e868406bae20961b505c5
Sun Kit Tsui <sunkittsui@gmail.com>
2023-01-01 18:03:39 -0500

    Removed redundant sorting before render

--- src/components/message/sent/MessagesSentList.tsx
+++ src/components/message/sent/MessagesSentList.tsx
@@ -12,11 +12,7 @@ const MessagesSentList: React.FC<MessagesSentListProps> = ({filteredMessagesSent
             id="message__sent__message-list"
             className="message__sent__message-list gen-container">
             {
-                filteredMessagesSent.sort((a, b) => {
-                    // sort in descending order by id
-                    // todo: sort by time sent
-                    return a.id > b.id ? -1 : a.id < b.id ? 1 : 0
-                }).map(message => {
+                filteredMessagesSent.map(message => {
                     return <MessageSentCard key={message.id} messageSent={message}/>
                 })
             }
